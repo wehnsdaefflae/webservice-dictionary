661a95660e384d5d238408c907650c25dd4e56b5
Mark <wernsdorfer+github@gmail.com>
2020-12-21 12:09:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import json
+import time
+from pathlib import Path
+from typing import Optional, Dict
+
+import flask
+from flask import jsonify, Response
+
+
+class Server:
+    app = flask.Flask(__name__)
+    app.config["DEBUG"] = True
+    path_file = Path("dictionary.json")
+    if path_file.is_file():
+        with path_file.open(mode="r") as file:
+            dictionary = json.load(file)
+    else:
+        dictionary = dict()
+
+    time_write_last = time.time()
+
+    @staticmethod
+    def _backup():
+        time_now = time.time()
+        if 10. < time_now - Server.time_write_last:
+            with Server.path_file.open(mode="w") as file:
+                json.dump(Server.dictionary, file, indent=2, sort_keys=True)
+            Server.time_write_last = time_now
+
+    @staticmethod
+    def pop(key: str) -> Optional[str]:
+        try:
+            value = Server.dictionary.pop(key)
+            Server._backup()
+            return value
+
+        except KeyError:
+            return None
+
+    @staticmethod
+    def get(key: str) -> Optional[str]:
+        return Server.dictionary.get(key)
+
+    @staticmethod
+    def complete() -> Dict[str, str]:
+        return Server.dictionary
+
+    @staticmethod
+    def set(key: str, value: str):
+        Server.dictionary[key] = value
+        Server._backup()
+
+
+@Server.app.route("/", methods=["GET"])
+def home() -> Response:
+    return jsonify(msg="<h1>Webservice Dictionary</h1><p>This site is a prototype API for a Python dictionary.</p>")
+
+
+@Server.app.route("/api/webservice-dictionary/v1/get/<string:key>")
+def get_value(key: str) -> Response:
+    value = Server.get(key)
+    if value is None:
+        return jsonify(msg=f"no value for key '{key:s}'")
+
+    return jsonify(value=value)
+
+
+@Server.app.route("/api/webservice-dictionary/v1/set/<string:key>/<string:value>")
+def set_value(key: str, value: str) -> Response:
+    Server.set(key, value)
+    return jsonify(msg="key mapped to value")
+
+
+@Server.app.route("/api/webservice-dictionary/v1/pop/<string:key>")
+def pop_value(key: str) -> Response:
+    value = Server.pop(key)
+    if value is None:
+        return jsonify(msg=f"no value for key '{key:s}'")
+
+    return jsonify(value=value)
+
+
+@Server.app.route("/api/webservice-dictionary/v1/complete/")
+def complete() -> Response:
+    dictionary = Server.complete()
+    return jsonify(dictionary)
+
+
+if __name__ == "__main__":
+    Server.app.run()
---
+++ wd_client.py
@@ -0,0 +1,86 @@
+import json
+from urllib.parse import quote
+from collections.abc import MutableMapping
+from typing import Iterator, TypeVar, Dict, Any, Tuple, Optional, Union
+import requests
+
+T = TypeVar('T')  # Any type.
+KT = TypeVar('KT')  # Key type.
+VT = TypeVar('VT')  # Value type.
+T_co = TypeVar('T_co', covariant=True)  # Any type covariant containers.
+V_co = TypeVar('V_co', covariant=True)  # Any type covariant containers.
+VT_co = TypeVar('VT_co', covariant=True)  # Value type covariant containers.
+T_contra = TypeVar('T_contra', contravariant=True)  # Ditto contravariant.
+
+# Internal type variable used for Type[].
+CT_co = TypeVar('CT_co', covariant=True, bound=type)
+
+
+class WSDict(MutableMapping):
+    _url = "http://127.0.0.1:5000/"
+    _set = _url + "api/webservice-dictionary/v1/set/"
+    _get = _url + "api/webservice-dictionary/v1/get/"
+    _pop = _url + "api/webservice-dictionary/v1/pop/"
+    _complete = _url + "api/webservice-dictionary/v1/complete/"
+
+    def __init__(self, seq: Optional[Union[Tuple[Any, Any], Dict[Any, Any]]] = None, **kwargs):
+        if seq is not None:
+            if isinstance(seq, dict):
+                for k, v in seq.items():
+                    self[k] = v
+
+            else:
+                for k, v in seq:
+                    self[k] = v
+
+        for k, v in kwargs.items():
+            self[k] = v
+
+    def __setitem__(self, k: KT, v: VT) -> None:
+        k_json = json.dumps(k)
+        v_json = json.dumps(v)
+        url = WSDict._set + f"{quote(k_json):s}/{quote(v_json):s}"
+        response = requests.get(url)
+
+    def __delitem__(self, v: KT) -> None:
+        k_json = json.dumps(v)
+        url = WSDict._pop + f"{quote(k_json):s}"
+        response = requests.get(url)
+
+    def __getitem__(self, k: KT) -> VT_co:
+        k_json = json.dumps(k)
+        url = WSDict._get + f"{quote(k_json):s}"
+        response = requests.get(url)
+        if not response.ok:
+            return None
+        response_dict = json.loads(response.text)
+        value_str = response_dict.get("value")
+        return json.loads(value_str)
+
+    def _get_response_dict(self) -> Dict[str, str]:
+        url = WSDict._complete
+        response = requests.get(url)
+        response_dict = json.loads(response.text)
+        return response_dict
+
+    def __len__(self) -> int:
+        response_dict = self._get_response_dict()
+        return len(response_dict)
+
+    def __iter__(self) -> Iterator[T_co]:
+        response_dict = self._get_response_dict()
+        dictionary = {json.loads(k): json.loads(v) for k, v in response_dict.items()}
+        return iter(dictionary)
+
+
+if __name__ == "__main__":
+    #s = '"asef"'
+    #print(unquote(s))
+    #print(quote(s))
+    d = WSDict()
+    d["testkey"] = "testvalue"
+    d[3] = [4, 6, 7]
+    _url = WSDict._complete
+    _response = requests.get(_url)
+
+    print()
